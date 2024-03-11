b5c8b952eacbfe806d499a84a29c62ae236dd7cb
bhaviljain <bhaviljain26@gmail.com>
2024-03-11 15:55:37 +0530

    Drop down

---
+++ Drop Down/src/App.jsx
@@ -0,0 +1,46 @@
+
+import { useState } from "react";
+
+ function App() {
+  // state to store the value of the country
+  const [country, setCountry] = useState(0);
+
+  const countries = [
+    { name: "India", value: "IN", cities: ["Delhi", "Mumbai"] },
+    { name: "Pak", value: "PK", cities: ["Lahore", "Karachi"] },
+    { name: "Bangladesh", value: "BG", cities: ["Dhaka", "Chittagong"] }
+  ];
+  
+
+  return (
+    <div className="App">
+      {/* 1st DropDown */}
+      <select
+        value={country}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setCountry([e.target.value]);
+        }}
+      >
+        {countries.map((item, index) => {
+          return (
+            <option key={index} value={index}>
+              {item.name}
+            </option>
+          );
+        })}
+      </select>
+
+      {/* 2nd DropDown */}
+
+      <select>
+        {countries[country] &&
+          countries[country].cities.map((item, index) => {
+            return <option value={index}>{item}</option>;
+          })}
+      </select>
+    </div>
+  );
+}
+export default App
+
